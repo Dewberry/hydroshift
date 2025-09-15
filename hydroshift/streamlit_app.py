15ac6daad5608b9500218a8d8ec9e86e6a627e98
sclaw <klawson@dewberry.com>
2025-09-15 11:53:04 +0000

    overhaul UI

--- hydroshift/streamlit_app.py
+++ hydroshift/streamlit_app.py
@@ -1,11 +1,19 @@
 import streamlit as st
 from session import init_session_state
+from PIL import Image
 
 from hydroshift._pages import changepoint, homepage, summary
 
 
 def navigator():
     """Make sidebar for multi-page navigation."""
+    # Define general style
+    im = Image.open("hydroshift/images/favicon.ico")
+    st.set_page_config(
+        page_title="HydroShift",
+        page_icon=im,
+    )
+
     # Initialize state
     if "session_id" not in st.session_state:
         init_session_state()
